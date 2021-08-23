f1fd46f8ffa5bc4bb8ffcce45d599e8c1046a5ab
skodange <89379691+skodange@users.noreply.github.com>
2021-08-23 14:43:28 +0530

    Create main.tf

---
+++ demo1/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+  aws = {
+     source="hashicorp/aws"
+     version="~> 3.27"
+    }
+  }
+  
+  required_versions = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region = "us-east-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami = "ami-07ffe7b5c785aaac1"
+  instance_type="t2.micro"
+  
+  tags = {
+    name = "Shreehari K"
+  }
+}
+
+
+
+
+
